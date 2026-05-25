a4423b296dd68af69c599027a1f92f77f324d43c
Ergi <ergimandija1@gmail.com>
2026-05-25 17:38:52 +0200

    exercise12/localStor

--- src/app/apiservice.ts
+++ src/app/apiservice.ts
@@ -1,6 +1,7 @@
 import { Injectable, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { UrlSegment } from '@angular/router';
+
 const initialLoginStatus = {
   loggedIn: false,
   loginError: false,
@@ -22,10 +23,10 @@ export interface UserData {
   error?: string;
 }
 
-export interface UserListItem{
-  id?:string;
-  name?:string;
-  group_id?:string;
+export interface UserListItem {
+  id?: string;
+  name?: string;
+  group_id?: string;
   error?: string;
 }
 
@@ -37,11 +38,18 @@ export interface UserListItem{
 export class ApiService {
   private apiUrl = `http://webp-ilv-backend.cs.technikum-wien.at/messenger/`;
 
-  private _loginStatus = signal(initialLoginStatus);
-  public loginStatus = this._loginStatus.asReadonly();
+  private _loginStatus;
+  public loginStatus ;
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) { 
+    const statusString =  localStorage.getItem("loginStatus");
+    this._loginStatus = (statusString!=null)? signal(JSON.parse(statusString)) :signal(initialLoginStatus);
+    console.log(this._loginStatus());
+    this.loginStatus = this._loginStatus.asReadonly();
+   }
+   
 
+   
   /**
    * logs in and updates signal loginStatus
    * @param username 
@@ -50,72 +58,70 @@ export class ApiService {
    */
   async login(username: string, password: string) {
     // TODO: implement login using fetch()
-    const userData = this.httpClient.post<UserData>(this.apiUrl+"/login.php", new URLSearchParams({
-                username_or_email: username,
-                password: password
-            }),
-            {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    }
-  }
-    );
-    userData.subscribe(data=>{
-        if(data.error){
-            console.log(data.error);
-        } else {
-          if(data.id != null && data.token != null){
-             this._loginStatus.set({
-              loggedIn: true,
-              loginError: false,
-              id: data.id,
-              username: username,
-              token: data.token
-            });
-            
-          }
+    const userData = this.httpClient.post<UserData>(this.apiUrl + "/login.php", new URLSearchParams({
+      username_or_email: username,
+      password: password
+    }),
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
         }
-    });
+      }
+    );
+    userData.subscribe(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        if (data.id != null && data.token != null) {
+          this._loginStatus.set({
+            loggedIn: true,
+            loginError: false,
+            id: data.id,
+            username: username,
+            token: data.token
+          });
+          localStorage.setItem("loginStatus", JSON.stringify(this._loginStatus()));
 
-    
-   
+        }
+      }
+    });
   }
 
-  getUserList(){
-    const token =  this._loginStatus().token;
-    const userList = this.httpClient.get<UserListItem[]| {error: string}>(this.apiUrl+"/get_users.php?token="+token+"&id="+this._loginStatus().id);
+  getUserList() {
+    const token = this._loginStatus().token;
+    const userList = this.httpClient.get<UserListItem[] | { error: string }>(this.apiUrl + "/get_users.php?token=" + token + "&id=" + this._loginStatus().id);
     return userList;
 
   }
 
 
-  async getConversation(user2Id:string){
+  async getConversation(user2Id: string) {
     const token = this._loginStatus().token;
     const data = await fetch(this.apiUrl + "get_conversation.php?token=" + token + "&user1_id=" + this._loginStatus().id + "&user2_id=" + user2Id);
-        const conversation = await data.json();
-        if (conversation.error) {
-            return { error: conversation.error };
-        }
-        return conversation;
+    const conversation = await data.json();
+    if (conversation.error) {
+      return { error: conversation.error };
+    }
+    return conversation;
   }
-  
-  async sendMessage(recieverId: string,message: string){
+
+  async sendMessage(recieverId: string, message: string) {
     const token = this._loginStatus().token;
     const response = await fetch(this.apiUrl + "send_message.php", {
-            method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: new URLSearchParams({
-                sender_id: this._loginStatus().id,
-                receiver_id: recieverId,
-                token: token,
-                message: message
-            })
-        });
-        const responseData = await response.json();
-        if (responseData.error) {
-            return { success: false, error: responseData.error };
-        }
-        return responseData;
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams({
+        sender_id: this._loginStatus().id,
+        receiver_id: recieverId,
+        token: token,
+        message: message
+      })
+    });
+    const responseData = await response.json();
+    if (responseData.error) {
+      return { success: false, error: responseData.error };
+    }
+    return responseData;
   }
 
 
